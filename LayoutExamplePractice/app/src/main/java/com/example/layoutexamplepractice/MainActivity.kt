8086c8895f16e3b0edacd9bab7164c2c76bae9c8
oh_jun <vhzkclq0705@naver.com>
2022-09-27 14:22:23 +0900

    Create UI programmatically

--- LayoutExamplePractice/app/src/main/java/com/example/layoutexamplepractice/MainActivity.kt
+++ LayoutExamplePractice/app/src/main/java/com/example/layoutexamplepractice/MainActivity.kt
@@ -2,12 +2,22 @@ package com.example.layoutexamplepractice
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View.generateViewId
+import android.widget.Button
 import android.widget.LinearLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
+
+//        create_UI()
+    }
+
+    // Create UI programmatically
+    fun create_UI() {
+        // Configure LinearLayout
 
         var linearLayout: LinearLayout = LinearLayout(this)
         var param = LinearLayout.LayoutParams(
@@ -17,6 +27,31 @@ class MainActivity : AppCompatActivity() {
         linearLayout.layoutParams = param
         linearLayout.orientation = LinearLayout.VERTICAL
 
+        // Configure first button
+
+        var button: Button = Button(this)
+        var buttonParam = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        )
+        buttonParam.gravity = Gravity.CENTER
+
+        button.id = generateViewId()
+        button.text = "첫 번째 버튼"
+        button.layoutParams = buttonParam
+
+        // Configure second button
+
+        var button2: Button = Button(this)
+        button2.id = generateViewId()
+        button2.text = "두 번째 버튼"
+        button2.layoutParams = buttonParam
+
+        // Add buttons
+
+        linearLayout.addView(button)
+        linearLayout.addView(button2)
         setContentView(linearLayout)
     }
+
 }
